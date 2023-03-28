5a737f7785f3bfdd744a551372b097eb6012b76a
Stepan Bahnik <bahniks@seznam.cz>
2023-03-28 17:02:29 +0200

    solution for equal bids started to be implemented

--- backend/selection/models.py
+++ backend/selection/models.py
@@ -16,16 +16,20 @@ class Session(models.Model):
 class Group(models.Model):
     group_number = models.AutoField(primary_key=True)   
     session = models.IntegerField(default=0)
-    participants = models.IntegerField(default=0)    
+    participants = models.IntegerField(default=0)        
+    auction_one = models.CharField(max_length=50, default="")  
+    auction_two = models.CharField(max_length=50, default="")  
+    auction_three = models.CharField(max_length=50, default="")  
 
 
 class Participant(models.Model):
     participant_id = models.CharField(max_length=50, default="")  
     group_number = models.IntegerField(default=0)
-    session = models.IntegerField(default=0)
+    session = models.IntegerField(default=0)    
     
 
 class Bid(models.Model):
     participant_id = models.CharField(max_length=50, default="")
     block = models.IntegerField(default=0)
     bid = models.IntegerField(default=0)
+    time = models.DateTimeField(auto_now_add=True)
--- backend/selection/views.py
+++ backend/selection/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 
 import random
 
-from .models import Bid, Session
+from .models import Bid, Session, Group, Participant
 
 
 @csrf_exempt 
@@ -19,14 +20,14 @@ def manager(request):
         if offer == "result":
             participant = Participant.objects.get(participant_id = participant_id)            
             group = Group.objects.get(group_number = participant.group_number)
-            all_members = Participant.objects.filter(group_number = participant.group_number)
-            bids = []
-            for p in all_members:
-                b = Bid.objects.get(participant_id = p.participant_id, block = block)
-                if p.participant_id == participant_id:
-                    myoffer = b.bid
-                bids.append(b.bid)
+            group_bids = getBids(participant_id, block)
+            bids = group_bids["bids"]
+            myoffer = group_bids["myoffer"]
             if len(bids) == group.participants:
+                bids.sort()
+                maxoffer = bids[len(bids) - 1]
+                secondoffer = bids[len(bids) - 2]
+                condition = "treatment" if myoffer == maxoffer else "control" # upravit pro rovnost                
                 response = "|".join(map(str, [condition, maxoffer, secondoffer, myoffer]))
             else:
                 response = ""
@@ -35,39 +36,61 @@ def manager(request):
             currentSession = Session.objects.latest('start')
             currentSession.participants += 1
             currentSession.save()
-            participant = Participant(participant_id = participant_id, group_number = -99, session = currentSession)
+            participant = Participant(participant_id = participant_id, group_number = -99, session = currentSession.session_number)
             participant.save()         
             return HttpResponse("ok")   
         else:
             bid = Bid(participant_id = participant_id, block = block, bid = offer)
             bid.save()
+            participant = Participant.objects.get(participant_id = participant_id) 
+            group = Group.objects.get(group_number = participant.group_number)
+            group_bids = getBids(participant_id, block)["bids"]
+            if len(group_bids) == group.participants:
+                pass # vybrat, kdo vyhral a ulozit do Group auction
             return HttpResponse("ok")
 
-        
+
+def getBids(participant_id, block):
+        participant = Participant.objects.get(participant_id = participant_id)            
+        group = Group.objects.get(group_number = participant.group_number)
+        all_members = Participant.objects.filter(group_number = participant.group_number)
+        bids = []
+        for p in all_members:
+            try:
+                b = Bid.objects.get(participant_id = p.participant_id, block = block)
+            except ObjectDoesNotExist:
+                pass
+            if p.participant_id == participant_id:
+                myoffer = b.bid
+            bids.append(b.bid)
+        return {"bids": bids, "myoffer": myoffer}
+
 
 @login_required(login_url='/admin/login/')
 def openSession(request):
     session = Session()
     session.save()
+    return HttpResponse("Session {} otevřena".format(session.session_number))
 
 
 @login_required(login_url='/admin/login/')
 def startSession(request):
     currentSession = Session.objects.latest('start')
-    participants = Participant.filter(session = currentSession)
+    participants = Participant.objects.filter(session = currentSession.session_number)
     number = len(participants)
     groups = number//4
     assignment = [i for i in range(number)]
     random.shuffle(assignment)    
     num = 0
     for i in range(groups):
-        group = Group(session = currentSession, participants = 4)
+        group = Group(session = currentSession.session_number, participants = 4)
         group.save()
         for j in range(4):
             p = participants[num]
             p.group_number = group.group_number
             p.save()
             num += 1
+    return HttpResponse("Session {} zahájena s {} participanty".format(currentSession.session_number, currentSession.participants))
 
 
 @login_required(login_url='/admin/login/')
